67df1701e2344dd046ad406702de79e90a9564b7
Mateusz <matmaq1@gmail.com>
2017-03-15 15:21:28 +0100

    Hello world

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,11 @@
  */
 public class Hello {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello world");
+
+
+        ;
     }
 }
---
+++ src/hello2.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Mateusz on 15.03.2017.
+ */
+public class hello2
+{
+    public static void main(String[] args) {
+        System.out.println("kappa");
+    }
+}
